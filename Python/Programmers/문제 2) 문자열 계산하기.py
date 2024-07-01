b43bb3b42a2c0fa0c2de8d612cc08ca5317ec28a
jw-park-github <165545220+jw-park-github@users.noreply.github.com>
2024-07-01 12:53:48 +0900

    Update 문제 2) 문자열 계산하기.py

--- Python/Programmers/문제 2) 문자열 계산하기.py
+++ Python/Programmers/문제 2) 문자열 계산하기.py
@@ -1,6 +1,6 @@
 # 출저: https://school.programmers.co.kr/app/courses/17584/curriculum/lessons/197236#part-46571
 # 유형: 문자열
-# 날짜: 6/29(o)
+# 날짜: 6/29(o), 7/1(o)
 '''
 ※ 문자열을 공백 기준으로 나누어 리스트로 변환 (my_string = my_string.split(' '))
 '''
