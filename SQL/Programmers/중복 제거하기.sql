c65e654815ff63e222bf7f13002483fd8d53bbb6
jw-park-github <165545220+jw-park-github@users.noreply.github.com>
2024-07-03 00:24:16 +0900

    Update 중복 제거하기.sql

--- SQL/Programmers/중복 제거하기.sql
+++ SQL/Programmers/중복 제거하기.sql
@@ -1,1 +1,9 @@
+# 출저: https://school.programmers.co.kr/learn/courses/30/lessons/59408
+# 유형: DISTINCT, COUNT, IS NOT NULL
+# 날짜: 7/2(x)
 
+SELECT COUNT(DISTINCT NAME) AS count
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL
+
+# DISTINCT 키워드: 중복된 값을 제거하고 고유한 값만을 선택하는데 사용.
